83e233e539b3357331667763f5e0b4178d50a224
Crispen LeBlanc <leblanc2042@gmail.com>
2024-09-30 10:44:58 -0600

    Add state to myHomePage and add navigationrail to myHomePage Widget

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//  The ChangeNotifier Class is like a megaphone that shouts out to your app whenever 
+//  The ChangeNotifier Class is like a megaphone that shouts out to your app whenever
 //  something important changes, so everyone stays in the loop and up-to-date!
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
@@ -35,27 +35,123 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+//  _ makes class private and enforced by the compiler
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    // MyAppState is always being watched for updates, and if there is any change
-    // to the appState, then the build() method will re-build
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('No widget for $selectedIndex');
+    }
+
+    return Scaffold(
+      body: Row(
+        children: [
+          //  Ensures child is not obscured by hardware notch or status bar
+          SafeArea(
+            child: NavigationRail(
+              //  Creates a Material Design navigation rail
+              extended: true, //  Controls labels next to icons
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                //  Notify the framework that the internal state of this object has changed.
+                setState(() {
+                  selectedIndex = value;
+                });
+              },
+            ),
+          ),
+
+          //  Expanded widgets are extremely useful in rows and columns—they let you
+          //  express layouts where some children take only as much space as they
+          //  need (SafeArea, in this case) and other widgets should take as much of
+          //  the remaining room as possible (Expanded, in this case).
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    return Scaffold(
-      body: Column( //  Defines Layout
-        children: [
-          Text('Here\'s a wicked random & AWESOME idea:'),  // ← Example change.
-          BigCard(pair: pair),
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
 
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
           ),
         ],
       ),
@@ -74,7 +170,7 @@ class BigCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //  Descendant widgets obtain the current theme's [ThemeData] object using [Theme.of].
-    //  When a widget uses [Theme.of], it is automatically rebuilt if the theme later 
+    //  When a widget uses [Theme.of], it is automatically rebuilt if the theme later
     //  changes, so that the changes can be applied.
     final theme = Theme.of(context);
     final style = theme.textTheme.displayMedium!.copyWith(
@@ -85,7 +181,11 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase, style: style),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
